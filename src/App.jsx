1b409b68026a450989b3423f242569e13cdcc627
Aaradhya Garg <Aaradhya.Garg@studentambassadors.com>
2024-02-22 00:08:48 +0530

    Add All Changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Aaradhya Garg",
+  title: "Student Ambassador",
+  email: "Aaradhya.Garg@studentambassadors.com",
+  gitHub: "AaradhyaGarg",
+  instagram: "",
+  linkedIn: "aaradhya-garg-548a50244",
   medium: "",
   twitter: "microsoft",
-  youTube: "Code",
+  youTube: "@Aaradhya_garg",
 };
 
 const primaryColor = "#4E567E";
