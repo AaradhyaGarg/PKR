f05fa70369fca7fdaf5063b64ef939c571c1a59c
Aaradhya Garg <Aaradhya.Garg@studentambassadors.com>
2024-02-22 00:27:49 +0530

    Add All Changes

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/volcano.jpeg";
+import image from "../images/volcano.jpg";
 
 const imageAltText = "I love Natural things";
 
